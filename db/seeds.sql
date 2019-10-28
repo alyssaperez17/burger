7f5e3fb89d14515cca8ba7d60d877a2369163d63
Alyssa Perez <aperez@censeohealth.com>
2019-10-27 20:27:58 -0500

    trying this assignment again

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Extra Patty Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Egg Burger', false);
